8a20427af816e6fa4501e536315b9be533ef20f9
Shu Fu Xiang <fxshu@sql.com.my>
2020-04-10 14:40:07 +0800

    Added "--quite" and "--single" options

--- tools/stan-sub/stan-sub.go
+++ tools/stan-sub/stan-sub.go
@@ -19,6 +19,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/nats-io/stan.go"
@@ -42,6 +43,8 @@ Subscription Options:
 	         (for more information: https://golang.org/pkg/time/#ParseDuration)
 	--durable <name>                Durable subscriber name
 	--unsubscribe                   Unsubscribe the durable on exit
+	--quite                         Only show message data
+	--single                        Only request for 1 message
 `
 
 // NOTE: Use tls scheme for TLS, e.g. stan-sub -s tls://demo.nats.io:4443 foo
@@ -64,6 +67,8 @@ func main() {
 	var durable string
 	var qgroup string
 	var unsubscribe bool
+	var quite bool
+	var single bool
 	var URL string
 
 	//	defaultID := fmt.Sprintf("client.%s", nuid.Next())
@@ -83,6 +88,9 @@ func main() {
 	flag.StringVar(&durable, "durable", "", "Durable subscriber name")
 	flag.StringVar(&qgroup, "qgroup", "", "Queue group name")
 	flag.BoolVar(&unsubscribe, "unsubscribe", false, "Unsubscribe the durable on exit")
+	flag.BoolVar(&quite, "quite", false, "Only show message data")
+	flag.BoolVar(&quite, "q", false, "Only show message data")
+	flag.BoolVar(&single, "single", false, "Only request for 1 message then close connection")
 
 	log.SetFlags(0)
 	flag.Usage = usage
@@ -106,13 +114,31 @@ func main() {
 	if err != nil {
 		log.Fatalf("Can't connect: %v.\nMake sure a NATS Streaming Server is running at: %s", err, URL)
 	}
-	log.Printf("Connected to %s clusterID: [%s] clientID: [%s]\n", URL, clusterID, clientID)
-
+	if !quite {
+		log.Printf("Connected to %s clusterID: [%s] clientID: [%s]\n", URL, clusterID, clientID)
+	}
 	subj, i := args[0], 0
 
-	mcb := func(msg *stan.Msg) {
+	var printfunc = func(msg *stan.Msg) {
 		i++
 		printMsg(msg, i)
+	}
+	if quite {
+		printfunc = func(msg *stan.Msg) {
+			log.Print(string(msg.Data))
+		}
+	}
+
+	signalChan := make(chan os.Signal, 1)
+
+	var mcb = func(msg *stan.Msg) {
+		printfunc(msg)
+	}
+	if single {
+		mcb = func(msg *stan.Msg) {
+			printfunc(msg)
+			signalChan <- syscall.SIGINT
+		}
 	}
 
 	startOpt := stan.StartAt(pb.StartPosition_NewOnly)
@@ -122,7 +148,9 @@ func main() {
 	} else if deliverLast {
 		startOpt = stan.StartWithLastReceived()
 	} else if deliverAll {
-		log.Print("subscribing with DeliverAllAvailable")
+		if !quite {
+			log.Print("subscribing with DeliverAllAvailable")
+		}
 		startOpt = stan.DeliverAllAvailable()
 	} else if startDelta != "" {
 		ago, err := time.ParseDuration(startDelta)
@@ -139,7 +167,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Printf("Listening on [%s], clientID=[%s], qgroup=[%s] durable=[%s]\n", subj, clientID, qgroup, durable)
+	if !quite {
+		log.Printf("Listening on [%s], clientID=[%s], qgroup=[%s] durable=[%s]\n", subj, clientID, qgroup, durable)
+	}
 
 	if showTime {
 		log.SetFlags(log.LstdFlags)
@@ -147,12 +177,13 @@ func main() {
 
 	// Wait for a SIGINT (perhaps triggered by user with CTRL-C)
 	// Run cleanup when signal is received
-	signalChan := make(chan os.Signal, 1)
 	cleanupDone := make(chan bool)
 	signal.Notify(signalChan, os.Interrupt)
 	go func() {
 		for range signalChan {
-			fmt.Printf("\nReceived an interrupt, unsubscribing and closing connection...\n\n")
+			if !quite {
+				fmt.Printf("\nReceived an interrupt, unsubscribing and closing connection...\n\n")
+			}
 			// Do not unsubscribe a durable on exit, except if asked to.
 			if durable == "" || unsubscribe {
 				sub.Unsubscribe()
